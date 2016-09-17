b9f0445fb5057ab5b36901c7849f82976658e638
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-09-17 18:08:09 -0400

    Initial architechture

--- query_processor/learner.py
+++ query_processor/learner.py
@@ -29,7 +29,7 @@ def train(dataset):
     query = '''
     PREFIX fb: <http://rdf.freebase.com/ns/>
     SELECT ?name where {
-        ?x fb:type.object.name "Albert Einstein"@EN .
+        ?x fb:type.object.name "Albert Einstein" .
         ?x fb:common.topic.alias ?name .
         FILTER (lang(?name) = "en")
     }
