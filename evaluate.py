04bba225da462bc3f7bb274a8381c2da66a40c4d
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-10-02 22:21:05 -0400

    Initial Architecture.

--- evaluate.py
+++ evaluate.py
@@ -11,6 +11,7 @@
 import sys
 import json
 import re
+import ast
 
 # Matches date formatted like this: 2/23/1836
 DATE_MDY_RE = re.compile(r'(\d+)/(\d+)/(\d+)')
@@ -94,8 +95,10 @@ with open(sys.argv[1]) as f:
   for line in f:
     tokens = line[:-1].split("\t")
     print tokens
-    gold = json.loads(tokens[1].decode('utf-8'))
-    predicted = json.loads(tokens[2].decode('utf-8'))
+    #gold = json.loads(tokens[1].decode('utf-8'))
+    #predicted = json.loads(tokens[2].decode('utf-8'))
+    gold = [n.strip() for n in ast.literal_eval(tokens[1])]
+    predicted = [n.strip() for n in ast.literal_eval(tokens[2])]
     recall, precision, f1 = computeF1(gold,predicted)
     if f1 == 1:
       nCorrect += 1
