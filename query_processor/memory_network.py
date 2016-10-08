aef2f4c3bbc0c38cd8d81d843b499e3812916503
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-10-08 03:00:25 -0400

    Memory Network

--- query_processor/memory_network.py
+++ query_processor/memory_network.py
@@ -55,7 +55,7 @@ class FMNLayer(object):
         H = []
         for f in self.F:
             (s, r, o) = f
-            gscore = np.dao(self.q, (sum(s) + sum(r)))
+            gscore = np.dot(self.q, (sum(s) + sum(r)))
             G.append(exp(gscore))
         Gsum = sum(G)
         for gscore in G:
