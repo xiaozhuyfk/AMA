7f6a8009f504b5c0fe3313916bea8650fec1773b
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-12-02 01:49:19 -0500

    cnn

--- condor.sh
+++ condor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #cd /home/hongyul/AMA; source ../.bashrc; python -m query_processor.rank_learner test webquestionstest
-cd /home/hongyul/AMA; source ../.bashrc; python -m query_processor.rank_learner model webquestionstrain
-#cd /home/hongyul/AMA; source ../.bashrc; python -m query_processor.rank_learner train webquestionstrain
+#cd /home/hongyul/AMA; source ../.bashrc; python -m query_processor.rank_learner model webquestionstrain
+cd /home/hongyul/AMA; source ../.bashrc; python -m query_processor.rank_learner train webquestionstrain
