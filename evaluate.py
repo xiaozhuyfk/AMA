0c777d86eefd67c20ecacaacca8e56f927441386
xiaozhuyfk <xiaozhuyfk@gmail.com>
2016-12-06 03:44:21 -0500

    cnn

--- evaluate.py
+++ evaluate.py
@@ -94,7 +94,7 @@ count=0
 with open(sys.argv[1]) as f:
   for line in f:
     tokens = line[:-1].split("\t")
-    print tokens
+    if len(tokens) <= 1: continue
     #gold = json.loads(tokens[1].decode('utf-8'))
     #predicted = json.loads(tokens[2].decode('utf-8'))
     gold = [n.strip() for n in ast.literal_eval(tokens[1])]
