1cb531ef31b4f123fcec160fa1e2b0b5a753a28d
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-02-27 10:12:18 -0500

    wiki

---
+++ submit_wiki_job.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def SubmitOneJob(self, idx):
+    lCmd = [
+        '/bos/usr0/suruchis/Files/qsub',
+        'python -m query_processor.rank_learner wiki %d' % idx
+    ]
+    CondorOut = subprocess.check_output(lCmd)
+    print "submitted: [%s]\n out [%s]" % (json.dumps(lCmd), CondorOut)
+
+for i in xrange(27):
+    idx = i + 1
+    SubmitOneJob(idx)
