8d1119c7ebacd2c7c320842ed3914bbdf3993d7d
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-02-27 08:07:05 -0500

    wiki

--- query_processor/wiki_extractor.py
+++ query_processor/wiki_extractor.py
@@ -225,6 +225,39 @@ class WikiAPIExtractor(object):
         sentences = [s for p in sentences for s in p]
         return sentences
 
+caps = "([A-Z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + caps + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(caps + "[.]" + caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + caps + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
 
 if __name__ == '__main__':
     abstract_xml = "/home/hongyul/AMA/wiki/enwiki/enwiki-latest-pages-articles1.xml"
@@ -275,9 +308,10 @@ if __name__ == '__main__':
                     content += part
             text = content
 
-            paragraphs = content.strip().split("\n")
-            sentences = [sent_tokenize(p) for p in paragraphs if p]
-            sentences = [s for p in sentences for s in p]
+            #paragraphs = content.strip().split("\n")
+            #sentences = [sent_tokenize(p) for p in paragraphs if p]
+            #sentences = [s for p in sentences for s in p]
+            sentences = split_into_sentences(text)
             for sent in sentences:
                 print sent
 
